e79299ba458800699df0aa18e047e23593cebe98
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-03 08:33:19 -0700

    add Meal model and AppComponent functionality for listing out names of each meal in the meals array

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,23 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Skeleton Angular2 App!</h1>
+      <h3 *ngFor="#meal of meals">{{ meal.name }}</h3>
     </div>
   `
 })
 export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+    this.meal = new Meal("Ultimate Breakfast", "Scrambeled eggs with mushrooms, toast and fruit on the side", 570),
+    this.meal = new Meal("Summer Salad", "Spinach, Strawberries, Avocado, Blueberries, Balsamic", 250),
+    this.meal = new Meal("Spicy Ramen", "Miso based broth with noodles, pork and an egg", 620)
+    ];
+  }
+}
 
+export class Meal {
+  constructor(public name: string, public description: string, public calories: number){
+
+  }
 }
